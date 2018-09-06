3952c8c954077aefc4ebddbcca143d5af43d46e3
Lazar Vasic <lazav94@gmail.com>
2018-09-06 16:16:54 +0200

    :rocket:

--- server/api/student/student.controller.js
+++ server/api/student/student.controller.js
@@ -4,20 +4,25 @@ const Student = require('./student.model');
 const getStudentById = async id => {
     return new Promise(async (resolve, reject) => {
         try {
+            console.log('getStudentById', id)
             const student = await Student.findOne({
                 id
             });
+            console.log('student ', student);
+
             if (student) {
                 resolve(student);
             } else {
                 const getUserInfo = require('../bot/messanger').getUserInfo;
                 const user = await getUserInfo(id);
+                console.log('user', user);
                 const newStudent = new Student({
                     id,
                     firstName: user.first_name,
 			        lastName: user.last_name
                 });
                 await newStudent.save();
+                console.log('new student', newStudent)
                 resolve(newStudent);
             }
         } catch (error) {
