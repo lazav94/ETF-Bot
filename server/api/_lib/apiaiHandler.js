8e2d3cd19fce090bb94cd565363a6d482c1cb148
Lazar Vasic <lazav94@gmail.com>
2018-09-07 16:35:49 +0200

    :rocket:

--- server/api/_lib/apiaiHandler.js
+++ server/api/_lib/apiaiHandler.js
@@ -319,7 +319,9 @@ const apiaiHandler = (sender, text) => {
     request.on('error', error => {
       console.log('API AI error');
       reject(error);
-    }
+    });
+
+
     request.end();
   });
 };
